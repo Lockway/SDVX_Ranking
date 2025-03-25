fc936c28dc4c6b85b1c074869561843b3f2e5588
Lockway <lchmax37@gmail.com>
2025-03-25 11:41:14 +0900

    Basic code

--- app/page.js
+++ app/page.js
@@ -1,95 +1,37 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+
+const dummyUsers = [
+  { rank: 1, username: 'MOL?LU', force: 21.987, sv: 'SV-5715-3145' },
+  { rank: 2, username: 'NVNL', force: 20.460, sv: 'SV-6638-4024' },
+  { rank: 3, username: 'RAIL', force: 20.374, sv: 'SV-5942-9468' },
+];
+
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <main>
+      <h1 style={{ textAlign: "center" }}>Game Ranking Board</h1>
+      <table className={ styles.Ranking }>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Username</th>
+            <th>Volforce</th>
+            <th>SV</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dummyUsers.map((user) => (
+            <tr key={user.rank}>
+              <td>{user.rank}</td>
+              <td>{user.username}</td>
+              <td>{user.force.toFixed(3)}</td>
+              <td>{user.sv}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
   );
 }
