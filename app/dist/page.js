23101e085cc3d7c64ada3ea8795500efc9b07204
Lockway <lchmax37@gmail.com>
2025-03-25 15:34:06 +0900

    navbar added

--- app/dist/page.js
+++ app/dist/page.js
@@ -1,4 +1,5 @@
 import dynamic from "next/dynamic";
+import Link from "next/link"
 
 const ForceChart = dynamic(() => import("./ForceChart"), { ssr: false });
 
@@ -11,6 +12,11 @@ const dummyUsers = Array.from({ length: 10000 }, (_, i) => ({
 export default function Dist() {
   return (
     <main>
+      <div className="navbar">
+        <Link href="/">Ranking</Link>
+        <Link href="/dist">VF Distribution</Link>
+      </div>
+
       <h1 style={{ textAlign: "center" }}>Volforce Distribution</h1>
       <div style={{ maxWidth: "1200px", margin: "0 auto" }}>
         <ForceChart users={dummyUsers} />
